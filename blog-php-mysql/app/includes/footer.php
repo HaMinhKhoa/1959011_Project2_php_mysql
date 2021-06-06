9b302d4d2b32bf22556f024fa6850adaf75c8463
HaMinhKhoa <80310599+HaMinhKhoa@users.noreply.github.com>
2021-06-07 00:14:15 +0700

    Add files via upload

---
+++ blog-php-mysql/app/includes/footer.php
@@ -0,0 +1,64 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Tree</span>Discussion</h1>
+        <p>
+          Tree Discussion is a opininion blog make people gather around to talk about somethings.
+          Simple as that.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 0973510183</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@treediscussion.com</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>More Info</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; Treediscussion.com | Designed by Me
+    </div>
+  </div>
+  <!-- // footer -->
